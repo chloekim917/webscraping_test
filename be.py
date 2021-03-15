e37abaa0df3b427c74642a8d90367de4c4e91d57
chloekim917 <chloekim917@gmail.com>
2021-03-15 14:08:22 -0400

    python function done

--- be.py
+++ be.py
@@ -1,47 +1,6 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support.expected_conditions import presence_of_element_located
-
-# #This example requires Selenium WebDriver 3.13 or newer
-# with webdriver.Chrome() as driver:
-#     wait = WebDriverWait(driver, 10)
-#     driver.get("https://google.com/ncr")
-#     driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
-#     first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
-#     print(first_result.get_attribute("textContent"))
-
-
-
-
-
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-
-# driver = webdriver.Chrome()
-
-# url = 'http://textfiles.com/stories/alissadl.txt'
-
-# driver.get(url)
-
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'u_cbox_area')))
-
-# html =  BeautifulSoup(driver.page_source, "html.parser")
-
-# result = html.find_all("span",{"class":"u_cbox_contents"})
-# for item in result:
-#     print(item.text)
-
-
-
-
-
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
 
 driver = webdriver.Chrome()
 
@@ -53,8 +12,28 @@ html =  BeautifulSoup(driver.page_source, "html.parser")
 for item in html:
     actual_text=item.text
 
-print(len(actual_text))
+# print(len(actual_text))
+driver.close() 
 
+def freq(actual_text): 
+  
+    #eliminate special characters except
+    new_string = re.sub(r'[^a-zA-Z0-9 ]','',actual_text)
+    # break the string into list of words
+    str_list = new_string.casefold().split()
+
+  
+    # gives set of unique words 
+    unique_words = set(str_list) 
+      
+    for words in unique_words : 
+        print(words,':', str_list.count(words)) 
+  
+# driver code 
+if __name__ == "__main__": 
+    actual_text
+    # calling the freq function 
+    freq(actual_text) 
 
 
 
